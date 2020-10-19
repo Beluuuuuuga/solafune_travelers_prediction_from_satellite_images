5d974ec161dc0a9a6cd79d6ae3c40ba8dc01cc96
kokiinoue <inoue.koki15@gmail.com>
2020-10-19 23:11:13 +0900

    add submit codes

--- src/models/train.py
+++ src/models/train.py
@@ -1,5 +1,11 @@
 import sys
 import pandas as pd
+import numpy as np
+import os
+import pathlib
+import glob
+
+import cv2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
@@ -10,6 +16,7 @@ if __name__ == "__main__":
 
     args = sys.argv
     model_name_prefix = args[1] # 保存のモデル名
+    epochs = int(args[2]) # epochs
 
     # csv読み込み
     train = pd.read_csv('traindataset_anotated.csv', names=["image","traveler"]) # headerあり読み込み
@@ -18,7 +25,16 @@ if __name__ == "__main__":
     target_size = (512, 512)
     batch_size = 4
 
-    train_datagen = ImageDataGenerator(rescale=1./255)
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=15,
+        shear_range=0.2,
+        horizontal_flip=True,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zca_whitening=True # ZCA白色化 
+    )
+
     train_datagenerator = train_datagen.flow_from_dataframe(
         train,
         "data/trainimage/image",
@@ -45,8 +61,7 @@ if __name__ == "__main__":
 
     history = model.fit(train_datagenerator,
 #                     steps_per_epoch=int(total_train//batch_size),
-                    # epochs=200,
-                    epochs=1,
+                    epochs=epochs,
                     validation_data=valid_datagenerator,
 #                     validation_steps=int(total_valid//batch_size),
                     verbose=1,
@@ -67,3 +82,23 @@ if __name__ == "__main__":
     fig_path = 'reports/' + model_name_prefix + '_loss.png'    
     plt.savefig(fig_path)
     plt.close()
+
+    # 推論
+    upload = pd.read_csv('uploadfile.csv', names=["image","traveler"]) # headerあり読み込み
+    evaluate_iter = pathlib.Path('data/evaluatemodel/image').glob('*.jpg')
+    for evaluate_path in evaluate_iter:
+        _path = str(evaluate_path)
+        _path = _path.split('/')[-1]
+        evaluate_img = cv2.imread(str(evaluate_path))
+        evaluate_img =  cv2.cvtColor(evaluate_img, cv2.COLOR_BGR2RGB)
+        evaluate_img =  cv2.resize(evaluate_img, (512, 512))
+        evaluate_img = np.array(evaluate_img / 255.)
+        evaluate_img = evaluate_img.reshape(1, 512, 512, 3)
+        train_y_log1p = model.predict(evaluate_img)[0][0]
+    #     train_y_log1p_inverse = np.exp(train_y_log1p) - 1
+        
+    #     upload.loc[upload['image'] == _path, 'traveler'] = int(train_y_log1p_inverse)
+        upload.loc[upload['image'] == _path, 'traveler'] = int(train_y_log1p)
+    
+    sub_csv_path = 'csvs/submit/' + model_name_prefix + '.csv'
+    upload.to_csv(sub_csv_path, header=False, index=False)
